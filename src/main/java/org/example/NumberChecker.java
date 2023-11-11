4a318418e85bd364afed271701e6f434ba9b9c45
xuan1006 <bob921006@gmail.com>
2023-11-11 18:32:07 +0800

    added src and pom,xml

--- src/main/java/org/example/NumberChecker.java
+++ src/main/java/org/example/NumberChecker.java
@@ -1,2 +1,12 @@
-package org.example;public class NumberChecker {
+package org.example;
+
+public class NumberChecker  {
+
+    public String check(int i) {
+        if(i>0)
+            return "Positive";
+        else if(i<0)
+            return "Negative";
+        return "Zero";
+    }
 }
--- src/test/java/org/example/NumberCheckerTests.java
+++ src/test/java/org/example/NumberCheckerTests.java
@@ -1,2 +1,23 @@
-package org.example;public class NumberChecker {
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumberCheckerTests {
+    @Test
+    public void positiveTest(){
+        NumberChecker numberChecker = new NumberChecker();
+        assertEquals("Positive", numberChecker.check(1));
+    }
+    @Test
+    public void negativeTest(){
+        NumberChecker numberChecker = new NumberChecker();
+        assertEquals("Negative", numberChecker.check(-1));
+    }
+    @Test
+    public void zeroTest(){
+        NumberChecker numberChecker = new NumberChecker();
+        assertEquals("Zero", numberChecker.check(0));
+    }
 }
